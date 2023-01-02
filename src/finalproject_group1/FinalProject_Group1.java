85dcc119763f3d17e0ada61297d4aa7c846739f6
amyrfrancisco <amyrfrancisco@192.168.1.25>
2023-01-02 22:20:32 +0800

    connect the database

--- src/finalproject_group1/FinalProject_Group1.java
+++ src/finalproject_group1/FinalProject_Group1.java
@@ -3,7 +3,8 @@ package finalproject_group1;
 public class FinalProject_Group1 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        //comment ni amyr
+        //isa pang comment
     }
     
 }
