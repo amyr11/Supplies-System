e2f1802cdd2ff88e8e54ef808bd5164d028ddf15
amyrfrancisco <amyrfrancisco@192.168.1.25>
2023-01-04 14:41:36 +0800

    removed con variable on main

--- src/finalproject_group1/FinalProject_Group1.java
+++ src/finalproject_group1/FinalProject_Group1.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class FinalProject_Group1 {
 
     public static void main(String[] args) {
-        Connection con = ConnectDB.Connect();
+        ConnectDB.Connect();
     }
     
 }
