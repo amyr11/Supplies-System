9c35c8cab6d3b0266a6b8ddf4a7e6b17293de5a8
amyrfrancisco <amyrfrancisco@192.168.1.25>
2023-01-04 18:41:34 +0800

    show main screen when login button is clicked

--- src/finalproject_group1/FinalProject_Group1.java
+++ src/finalproject_group1/FinalProject_Group1.java
@@ -1,9 +1,13 @@
 package finalproject_group1;
 
+import screens.*;
+
 public class FinalProject_Group1 {
 
     public static void main(String[] args) {
-        ConnectDB.Connect();
+//        ConnectDB.Connect();
+        LoginScreen loginScreen = new LoginScreen();
+        loginScreen.setVisible(true);
     }
     
 }
