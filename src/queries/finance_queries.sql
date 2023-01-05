255d449f38daa57ad60e74c5c77bc2742881372e
amyrfrancisco <amyrfrancisco@192.168.1.25>
2023-01-05 15:32:07 +0800

    added more dummy data

--- src/queries/finance_queries.sql
+++ src/queries/finance_queries.sql
@@ -1,7 +1,24 @@
 -- Revenue per month
-SELECT MONTH(transaction_date) AS transaction_month, 
-       SUM(cost) AS revenue
-FROM Transactions
-WHERE status = 'delivered'
-      AND transaction_type = 'sale'
-GROUP BY MONTH(transaction_date);
+-- SELECT MONTH(transaction_date) AS transaction_month, 
+--        SUM(cost) AS revenue
+-- FROM Transactions
+-- WHERE status = 'delivered'
+--       AND transaction_type = 'sale'
+-- GROUP BY MONTH(transaction_date);
+
+-- Drafts
+-- SELECT product_name, SUM(quantity) as remaining_stocks
+-- FROM transactions
+-- WHERE status = 'delivered'
+-- GROUP BY product_name
+-- ORDER BY remaining_stocks DESC;
+
+-- SELECT SUM(cost) as profit
+-- FROM transactions
+-- WHERE status = 'delivered';
+
+-- SELECT product_name, SUM(cost) as profit
+-- FROM transactions
+-- WHERE status = 'delivered'
+-- GROUP BY product_name
+-- ORDER BY profit DESC;
